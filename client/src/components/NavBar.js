35311d895ccf15bd29b3d6f294aef38536d8f5fd
Karl Holub <holub008@umn.edu>
2018-11-26 20:53:58 -0500

    bumping nodemon to remove event-stream vulnerability

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,16 +1,29 @@
 import React from 'react';
 
 import {Link} from 'react-router-dom';
+import {Menu,Grommet,Box} from 'grommet';
+import { grommet } from "grommet/themes";
+
 
 import '../styles/NavBar.css';
 
 function NavBar(props) {
     return(
-        <div className="navbar">
+        <Grommet theme={grommet}>
+        <Box direction="row-responsive" justify="between"
+        style={{backgroundColor:"rgba(57, 62, 69, 0.82)"}}>
                 <Link to="/">
                     <img className="navbar-logo" src="/images/logo.png" alt=""/>
                 </Link>
-        </div>
+            <Menu
+                label="Actions"
+                items={[
+                    { label: "Launch", onClick: () => {} },
+                    { label: "Abort", onClick: () => {} }
+                ]}
+            />
+        </Box>
+        </Grommet>
     );
 }
 
