e8fe82c4dca3bdc99de85cb0f28a52073311a7cd
Karl Holub <holub008@umn.edu>
2018-12-07 01:03:09 -0500

    naive elo ranking implemented in R sim

--- offline/scoring/simulate/ranking_experiments.R
+++ offline/scoring/simulate/ranking_experiments.R
@@ -43,8 +43,62 @@ simulate_races <- function(population,
   bind_rows()
 }
 
+naive_elo <- function(results,
+                      log_odds_oom_differential = 400,
+                      k_factor = 1,
+                      default_score = 1000) {
+  results$score <- 1000
+  score_history <- data.frame()
+  for (ix in sort(unique(results$iteration))) {
+    race_results <- results %>% 
+      filter(iteration == ix)%>%
+      mutate(
+        race_rank = rank(time)
+      )
+    
+    updated_racers <- data.frame()
+    for (update_racer_id in race_results$racer_id) {
+      racer <- race_results %>% 
+        filter(racer_id == update_racer_id) %>%
+        mutate(
+          linear_scale_score = 10 ^ (score / log_odds_oom_differential)
+        ) 
+
+      score_increment <- race_results %>%
+        filter(racer_id != update_racer_id) %>%
+        mutate(
+          linear_scale_competitor_score = 10 ^ (score / log_odds_oom_differential),
+          p_racer = racer$linear_scale_score / (linear_scale_competitor_score + racer$linear_scale_score),
+          outcome = ifelse(race_rank < racer$race_rank, 1, 0),
+          score_change = k_factor * (outcome - p_racer)
+        ) %>%
+        pull(score_change) %>%
+        sum()
+      
+      racer$updated_score <- racer$score + score_increment
+      
+      updated_racers <- bind_rows(updated_racers, racer)
+    }
+    
+    print(head(updated_racers))
+    
+    results <- results %>%
+      left_join(updated_racers %>% select(racer_id, updated_score), by = c('racer_id' = 'racer_id')) %>%
+      mutate(
+        score = ifelse(!is.na(updated_score), updated_score, score)
+      ) %>%
+      select(-updated_score)
+    score_history <- bind_rows(score_history, updated_racers %>% select(racer_id, updated_score))
+  }
+  
+  list(race_results,
+       score_history)
+}
+
 ###########################
 ## start control flow
 ###########################
 population <- generate_population()
 all_results <- simulate_races(population)
+
+elo_scores <- naive_elo(all_results)
